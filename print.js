527b11bc8182a22a71861a9d2036dfb93ac9aee5
Matt Hensley <matt.hensley777@gmail.com>
2017-03-28 13:35:11 -0500

    car card background color changing

--- print.js
+++ print.js
@@ -1,21 +1,22 @@
 var CarLot = (function(cars) {
+    var currentCar;
 
-  //  events if/else statements. 
- carContainer.addEventListener("click", selectCar);
+    //  events if/else statements. 
+    carContainer.addEventListener("click", selectCar);
 
-function selectCar() {
-var cars = document.getElementsByClassName("container");
+    function selectCar(e) {
+        var cars = document.getElementsByClassName("container");
+console.log(container)
+        for (var j = 0; j < cars.length; j++) {
+            cars[j].style.border = "solid black 5px";
+        }
 
-for (var j = 0; j < cars.length; j++) {
-        cars[j].style.border = "solid black 5px";
+        if (e.target.classList.contains("container")) {
+            e.target.style.backgroundColor = "lightgrey";
+            currentCar = e.target;
+        }
+
     }
-
-    // if (cars.target.contains("container")) {
-    //     cars.target.style.border = "solidBlack 5px";
-    //     cars = carLot.target;
-    // } 
-    
-}
 
     return cars;
 })(CarLot || {});
