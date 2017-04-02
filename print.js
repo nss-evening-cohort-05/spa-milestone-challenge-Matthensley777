75e50b2fc03df9199fafbd5803c2d1688162700c
Matt Hensley <matt.hensley777@gmail.com>
2017-04-02 13:38:05 -0500

    got text bound to box working not sure if it's what we are going for though

--- print.js
+++ print.js
@@ -26,5 +26,15 @@ var CarLot = (function(cars) {
         document.getElementById("text").focus();
     }
 
+document.getElementById("text").addEventListener("keyup", carBoundToText);
+
+function carBoundToText(e) {
+    if (e.keyCode === 13) {
+        document.getElementById("text").value = "";
+    } else {
+        currentCar.innerHTML = document.getElementById("text").value;
+    }
+}
+
     return cars;
 })(CarLot || {});
