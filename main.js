1cbe134e89b154a4380d31b460abeece571aafe9
Matt Hensley <matt.hensley777@gmail.com>
2017-04-17 21:12:03 -0500

    code working

--- main.js
+++ main.js
@@ -1,32 +1,24 @@
 var CarLot = (function(cars) {
 
-    cars.selectCar = function(carType) {
+
+    document.getElementById("text").addEventListener("keyup", carBoundToText);
+
+    function carBoundToText(e) {
+        var currentCar = CarLot.getCurrentCar();
+        if (e.keyCode === 13) {
+            document.getElementById("text").value = "";
+        } else {
+            currentCar.innerHTML = document.getElementById("text").value;
+        }
     }
 
-
-
-            // Loop over the inventory and populate the page
-
-
-            document.getElementById("text").addEventListener("keyup", carBoundToText);
-
-            function carBoundToText(e) {
-                var currentCar = CarLot.getCurrentCar();
-                if (e.keyCode === 13) {
-                    document.getElementById("text").value = "";
-                } else {
-                    currentCar.innerHTML = document.getElementById("text").value;
-                }
-            }
-
-        // Load the inventory and send a callback function to be
-        // invoked after the process is complete
-        CarLot.loadInventory();
+     
+    CarLot.loadInventory();
 
 
     return cars;
 
-    })(CarLot || {});
+})(CarLot || {});
 
 
 
--- print.js
+++ print.js
@@ -11,31 +11,35 @@ var CarLot = (function(cars) {
         var cars = document.getElementsByClassName("mainCard");
                 console.log(cars)
 
+            document.getElementById("text").value = "";
+
+
         for (var i = 0; i < cars.length; i++) {
             cars[i].classList.remove("selected");
+            
 
         }
         if (e.target.classList.contains("mainCard")) {
             e.target.classList.add("selected");
+            e.target.childNodes[4].value = "";
             currentCar = e.target.childNodes[0].childNodes[4];
         } else if (e.target.classList.contains("child")) {
             e.target.parentNode.classList.add("selected");
+            e.target.parentNode.value = "";
             currentCar = e.target.parentNode.childNodes[0].childNodes[4];
         } else if (e.target.classList.contains("grandChild")) {
             currentCar = e.target.parentNode.parentNode.childNodes[0].childNodes[4];
             e.target.parentNode.parentNode.classList.add("selected");
+            e.target.parentNode.parentNode.value = "";
         } 
         document.getElementById("text").focus();
     }
 
+    
 
     cars.getCurrentCar = function() {
         return currentCar;
     }
-
-    
-
-
 
 
     return cars;
