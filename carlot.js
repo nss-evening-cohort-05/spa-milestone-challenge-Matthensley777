9cd9ec22e1110ccc7c5a5abb227e3f77903f641c
Matt Hensley <matt.hensley777@gmail.com>
2017-05-01 16:01:41 -0500

    working

--- carlot.js
+++ carlot.js
@@ -8,14 +8,13 @@ var CarLot = (function() {
         for (var i = 0; i < carData.length; i++) {
             currentCar = carData[i];
             carString += `<div class="col-sm-6 col-md-4 mainCard">`;
-            carString += `<div class="thumbnail">`;
             carString += `<div id="container" class="child">`;
             carString += `<h3 class="grandChild" > Make: ${currentCar.make}</h3>`;
             carString += `<p class="grandChild"> Model: ${currentCar.model}</p>`;
             carString += `<p class="grandChild"> Year: ${currentCar.year}</p>`;
             carString += `<p class="grandChild"> Price: $${currentCar.price}</p>`;
             carString += `<p class="grandChild" id="description"> Description: ${currentCar.description}</p>`;
-            carString += `</div></div></div>`;
+            carString += `</div></div>`;
 
             carContainer.innerHTML = carString;
         }
